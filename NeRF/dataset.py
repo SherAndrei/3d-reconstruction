0036c196c638dd63f994f5e8c57a6364cab28f47
SherAndrei <welldryus@gmail.com>
2025-02-28 00:48:21 +0300

    dataset.py now displays several pictures on one page

--- NeRF/dataset.py
+++ NeRF/dataset.py
@@ -4,31 +4,57 @@ import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-def display(images):
-  num_images = images.shape[0]
+def display_square(images, grid_size):
+    """
+    Display images in a grid layout with pagination.
+    
+    Parameters:
+        images (np.ndarray): Array of images to display.
+        grid_size (int): The number of images per row/column in the grid.
+    """
 
-  fig, axs = plt.subplots(2, 3)
+    assert len(images.shape) == 4 # expect (num_images, width, height, rgb)
 
-  def display_random_pictures():
-    for ax in axs.flat:
-      random_index = np.random.randint(low=0, high=num_images)
-      ax.imshow(images[random_index])
-      ax.axis('off')  # Hide axes for better visualization
+    current_page = 0
+    num_images = images.shape[0]
+    images_on_page = grid_size * grid_size
+    num_pages = (num_images + images_on_page - 1) // images_on_page
 
-    plt.draw()
+    fig, axs = plt.subplots(grid_size, grid_size, figsize=(grid_size, grid_size))
 
-  def on_key_press(event):
-    display_random_pictures()
+    def update_display(idx):
+        nonlocal current_page
+        current_page = idx
+        start = idx * images_on_page
+        end = start + images_on_page
+        current_images = images[start:end]
+        for ax, img in zip(axs.flat, current_images):
+            ax.clear()
+            ax.axis('off')
+            ax.imshow(img)
+        for ax in axs.flat[len(current_images):]:
+            ax.clear()
+            ax.axis('off')
+        plt.draw()
 
-  fig.canvas.mpl_connect('key_press_event', on_key_press)
-  display_random_pictures()
-  plt.tight_layout()  # Adjust layout for better spacing
-  plt.show()
+    def on_key_press(event):
+        nonlocal current_page
+        if event.key == 'right':
+            new_index = (current_page + 1) % num_pages
+            update_display(new_index)
+        elif event.key == 'left':
+            new_index = (current_page - 1) % num_pages
+            update_display(new_index)
+
+        fig.canvas.draw_idle()
+
+    fig.canvas.mpl_connect('key_press_event', on_key_press)
+    update_display(0)
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == '__main__':
-  url = "http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz"
-  data = keras.utils.get_file(origin=url)
-
-  data = np.load(data)
-  display(data['images'])
-
+    URL = "http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz"
+    file_path = keras.utils.get_file(origin=URL)
+    data = np.load(file_path)
+    display_square(data['images'], 5)
